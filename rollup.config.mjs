bb213b2390d12d048f08aaf477521fd5269c7174
luoxiangfan <infiniment@163.com>
2024-10-11 15:21:55 +0800

    fix: error occured when using from CDN

---
+++ rollup.config.mjs
@@ -0,0 +1,53 @@
+import * as fs from 'fs'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+
+const loadJSON = (path) =>
+  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)))
+
+const pkg = loadJSON('./package.json')
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      // config for <script type="module">
+      {
+        file: pkg.module,
+        format: 'esm'
+      },
+      // config for <script nomodule>
+      {
+        file: pkg.browser,
+        format: 'umd',
+        name: 'Cookies',
+        noConflict: true,
+        banner: ';'
+      }
+    ],
+    plugins: [
+      typescript()
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [
+      // config for <script type="module">
+      {
+        file: pkg.module.replace('.mjs', '.min.mjs'),
+        format: 'esm'
+      },
+      // config for <script nomodule>
+      {
+        file: pkg.browser.replace('.js', '.min.js'),
+        format: 'umd',
+        name: 'Cookies',
+        noConflict: true
+      }
+    ],
+    plugins: [
+      terser(),
+      typescript()
+    ]
+  }
+]
